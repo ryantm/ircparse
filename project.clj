85fbbba5ea3a00fe268b28eee43cf685726af979
Ryan Mulligan <ryan@ryantm.com>
2014-02-16 14:27:34 -0800

    add project descriptions, remove core.match dependency

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject ircparse "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A parser for IRC messages."
+  :url "https://github.com/talklibre/ircparse"
+  :license {:name "CC0 1.0 Universal"
+            :url "http://creativecommons.org/publicdomain/zero/1.0/legalcode"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [instaparse "1.2.14"]
-                 [org.clojure/core.match "0.2.1"]])
+                 [instaparse "1.2.14"]])
