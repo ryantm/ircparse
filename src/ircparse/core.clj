1ca3a816711f899bf5f6723e85c261ede514d9c0
Ryan Mulligan <ryan@ryantm.com>
2014-02-16 19:19:44 -0800

    improvements to parser and extractions
    * chunk user prefix string terminals together
    * extract user prefix data
    * rename prefix extraction to server-hostname
    * fix bug in hostname regex that did not allow two letter parts of
      domain names

--- src/ircparse/core.clj
+++ src/ircparse/core.clj
@@ -9,42 +9,79 @@
 (defn rule? [rule subtree]
   (= rule (first subtree)))
 
-(defn match-rule [rules rule]
-  (let [matching-rule (first (filter (partial rule? rule) rules))]
+(defn match-rule [subtree rule]
+  (let [matching-rule (first (filter (partial rule? rule) subtree))]
     (rest matching-rule)))
 
-(defn get-server-hostname [subtree]
-  (-> (list subtree)
+(defn get-client-user [tree]
+  (-> (list tree)
+      (match-rule :message)
+      (match-rule :prefix)
+      (match-rule :user)
+      first))
+
+(defn get-client-nickname [tree]
+  (-> (list tree)
+      (match-rule :message)
+      (match-rule :prefix)
+      (match-rule :nickname)
+      first))
+
+(defn get-client-host [tree]
+  (-> (list tree)
+      (match-rule :message)
+      (match-rule :prefix)
+      (match-rule :host)
+      (match-rule :hostname)
+      first))
+
+(defn get-server-hostname [tree]
+  (-> (list tree)
       (match-rule :message)
       (match-rule :prefix)
       (match-rule :servername)
       (match-rule :hostname)
       first))
 
-(defn get-command [subtree]
-  (-> (list subtree)
+(defn get-command [tree]
+  (-> (list tree)
       (match-rule :message)
       (match-rule :command)
       first))
 
-(defn get-trailing-params [subtree]
-  (-> (list subtree)
+(defn get-trailing-params [tree]
+  (-> (list tree)
       (match-rule :message)
       (match-rule :params)
       (match-rule :trailing)
       first))
 
+(defn- no-nil-map [kv-list]
+  (into {} (remove (comp nil? second) kv-list)))
+
 (defn message
   "Takes a full IRC message, including the CR LF characters and returns
-a hash with extracted information with these keys:
+a map with extracted information with some of these keys:
 
 * command
-* prefix
+* server-hostname
 * trailing-params
-* parse-tree"
+* client-user
+* client-nickname
+* client-host
+* parse-tree
+
+If it fails to parse the message, it returns a map with a single key,
+:failure, which is an Instaparse failure object."
   [message]
-  (let [parse-tree (parser message)]
-    {:command (get-command parse-tree)
-     :prefix (get-server-hostname parse-tree)
-     :trailing-params (get-trailing-params parse-tree)
-     :parse-tree parse-tree}))
+  (let [instaparse-return (parser message)]
+    (if (insta/failure? instaparse-return)
+      {:failure (insta/get-failure instaparse-return)}
+      (no-nil-map
+       [[:command (get-command instaparse-return)]
+        [:server-hostname (get-server-hostname instaparse-return)]
+        [:trailing-params (get-trailing-params instaparse-return)]
+        [:client-user (get-client-user instaparse-return)]
+        [:client-nickname (get-client-nickname instaparse-return)]
+        [:client-host (get-client-host instaparse-return)]
+        [:parse-tree instaparse-return]]))))
--- test/ircparse/core_test.clj
+++ test/ircparse/core_test.clj
@@ -1,27 +1,39 @@
 (ns ircparse.core-test
   (:require [clojure.test :refer :all]
-            [ircparse.core :refer :all]))
+            [ircparse.core :refer :all]
+            [clojure.pprint :refer [pprint]]))
 
 (deftest ircparse-test
-  (testing "Can parse a notice."
+  (testing "a malformed message"
+    (is (contains? (message "HI") :failure)))
+  (testing "can parse some messages"
+    (let [parsed (message ":ryantm!~ryantm@ip98-167-44-17.lv.lv.cox.net QUIT :Quit: ryantm\r\n")]
+      (is (= (:command parsed)
+             "QUIT"))
+      (is (= (:client-nickname parsed)
+             "ryantm"))
+      (is (= (:client-user parsed)
+             "~ryantm"))
+      (is (= (:client-host parsed)
+             "ip98-167-44-17.lv.lv.cox.net")))
     (let [parsed (message ":barjavel.freenode.net NOTICE * :*** Found your hostname\r\n")]
       (is (= (:command parsed)
              "NOTICE"))
-      (is (= (:prefix parsed)
+      (is (= (:server-hostname parsed)
              "barjavel.freenode.net"))
       (is (= (:trailing-params parsed)
              "*** Found your hostname")))
     (let [parsed (message ":hubbard.freenode.net NOTICE * :*** Looking up your hostname...\r\n")]
       (is (= (:command parsed)
              "NOTICE"))
-      (is (= (:prefix parsed)
+      (is (= (:server-hostname parsed)
              "hubbard.freenode.net"))
       (is (= (:trailing-params parsed)
              "*** Looking up your hostname...")))
     (let [parsed (message ":cameron.freenode.net 433 * rtm :Nickname is already in use\r\n")]
       (is (= (:command parsed)
              "433"))
-      (is (= (:prefix parsed)
+      (is (= (:server-hostname parsed)
              "cameron.freenode.net"))
       (is (= (:trailing-params parsed)
              "Nickname is already in use"))
