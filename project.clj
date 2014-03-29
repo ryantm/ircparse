ad47f97ce60ff7e4317535497cf0fd50adfa63d2
Ryan Mulligan <ryan@ryantm.com>
2014-03-28 22:08:03 -0700

    update dependencies to latest versions

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/talklibre/ircparse"
   :license {:name "CC0 1.0 Universal"
             :url "http://creativecommons.org/publicdomain/zero/1.0/legalcode"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [instaparse "1.2.14"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [instaparse "1.3.0"]])
